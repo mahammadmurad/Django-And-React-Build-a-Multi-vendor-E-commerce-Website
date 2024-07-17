b7d6d351a29bbe3259e80a7549696e1776602256
mahammadmurad <mahammad.muradd@gmail.com>
2024-07-17 14:54:53 +0400

    Custom user model created

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -133,3 +134,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'userauths.User'
--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from userauths.models import User, Profile
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'full_name']
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["full_name", ]
+
 
 # Register your models here.
