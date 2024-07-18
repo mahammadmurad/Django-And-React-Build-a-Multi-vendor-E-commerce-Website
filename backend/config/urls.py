ebef48ee5a1d741ab7b3de563750f273dbadb28a
mahammadmurad <mahammad.muradd@gmail.com>
2024-07-18 15:13:57 +0400

    drf-yasg configured

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,10 +18,30 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='E-commerce Backend API',
+        default_version= 'v1',
+        description= ' This is the documentation',
+        terms_of_service= 'oxu.az',
+        contact= openapi.Contact(email='mahammad.muradd@gmail.com'),
+        license=openapi.License(name='ISO'),
+    ),
+    public=True,
+    permission_classes = (permissions.AllowAny,)
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
+    path('api/v1/', include('api.urls')),
+    
+    #Documentation
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
