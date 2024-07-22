c6f1bbce81eed26539224c60c7518e13dc527ff0
mahammadmurad <mahammad.muradd@gmail.com>
2024-07-22 16:28:07 +0400

    React Utility Functions

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from userauths import views as userauths_views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),
-    path('user/register/', userauths_views.RegisterView.as_view()),
+    path("user/token/", userauths_views.MyTokenObtainPairView.as_view()),
+    path("user/register/refresh", TokenRefreshView.as_view()),
+    path("user/register/", userauths_views.RegisterView.as_view()),
 ]
